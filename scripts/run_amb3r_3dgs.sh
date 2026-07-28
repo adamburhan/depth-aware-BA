e03b7c87b2c31eb5bf1a3defb4543bb9d19d822a
Adam Burhan <adam14011999@gmail.com>
2026-07-28 02:38:25 -0400

    3dgs training scannetpp

--- scripts/run_amb3r_3dgs.sh
+++ scripts/run_amb3r_3dgs.sh
@@ -2,10 +2,10 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=32G
 #SBATCH --time=4:00:00
-#SBATCH --job-name=tt_amb3r_3dgs
-#SBATCH --output=/network/scratch/a/adam.burhan/logs/tt_3DGS_amb3r_%A_%a.out
+#SBATCH --job-name=snpp_amb3r_3dgs
+#SBATCH --output=/network/scratch/a/adam.burhan/logs/snpp_3DGS_amb3r_%A_%a.out
 #SBATCH --gres=gpu:l40s:1
-#SBATCH --array=0-14
+#SBATCH --array=0-2
 
 set -euo pipefail
 
@@ -14,14 +14,17 @@ modes=(
     unimodal
     gmm
 )
+# sequences=(
+#     Barn
+#     Caterpillar
+#     # Church
+#     # Courthouse
+#     Ignatius
+#     Meetingroom
+#     Truck
+# )
 sequences=(
-    Barn
-    Caterpillar
-    # Church
-    # Courthouse
-    Ignatius
-    Meetingroom
-    Truck
+    09c1414f1b
 )
 
 n_seq=${#sequences[@]}
@@ -32,14 +35,13 @@ module load cuda/12.1.1
 source $SCRATCH/envs/gs3d/bin/activate
 repo_root=$HOME/repos/gaussian-splatting
 
-data_root=$SCRATCH/experiments/depth-aware-ba/3dgs_data
-out_root=$SCRATCH/experiments/depth-aware-ba/3dgs_out
+data_root=$SCRATCH/experiments/depth-aware-ba/3dgs_data_snpp
+out_root=$SCRATCH/experiments/depth-aware-ba/3dgs_out_snpp
 out=$out_root/${seq}_${mode}
 mkdir -p $out_root
 
 cd $repo_root
 python train.py -s $data_root/${seq}_${mode} -m $out \
-    --eval -r 1 --save_iterations 7000 30000 --test_iterations 7000 30000 --quiet --disable_viewer
-python render.py -m $out --iteration 7000 --skip_train
+    --eval -r 1 --save_iterations 30000 --test_iterations 30000 --quiet --disable_viewer
 python render.py -m $out --iteration 30000 --skip_train
 python metrics.py -m $out
