253b90427ee14f4b5ecf4acd96945cccb6459833
Adam Burhan <adam14011999@gmail.com>
2026-07-27 23:25:18 -0400

    scannet script

--- scripts/run_amb3r_processing.sh
+++ scripts/run_amb3r_processing.sh
@@ -19,6 +19,7 @@ seq=${sequences[$SLURM_ARRAY_TASK_ID]}
 
 repo_root=$HOME/repos/depth-aware-BA/
 cd $repo_root
+source .venv/bin/activate
 python scripts/process_amb3r_outputs.py \
 	--npz ~/scratch/datasets/scannetpp/data/amb3r/$seq/scene_${seq}_results.npz \
 	--out ~/scratch/datasets/scannetpp/data/amb3r/$seq \
